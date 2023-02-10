f4eb57d7725cebcad19eec13f3f64867bc6d3a66
Ingrid Fonoy <ingridsfonoyd@gmail.com>
2023-02-09 19:51:05 -0600

    exercise1and2Done

---
+++ INTRO-WEB-7/js/exercise02.js
@@ -0,0 +1,11 @@
+function extraFact() {
+    alert('My fav dessert is Apple Pie!')
+}
+
+function extraBorn() {
+    alert('I was born in an island!')
+}
+
+function extraMusic() {
+    alert('My fav music is K-POP!')
+}
