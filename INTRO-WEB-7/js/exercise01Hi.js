760fdc06daa6f0aad7f502b34c098d275a6c95ed
Ingrid Fonoy <ingridsfonoyd@gmail.com>
2023-02-09 18:55:13 -0600

    exercise1IntroWeb7Done

---
+++ INTRO-WEB-7/js/exercise01Hi.js
@@ -0,0 +1,4 @@
+function show() {
+    let askName = prompt( "Hellow, what is your name?" );
+    document.write( "Hola " + askName );
+}
--- INTRO-WEB-7/js01/variables01.js
+++ INTRO-WEB-7/js01/variables01.js
@@ -54,4 +54,3 @@ function imc() {
         console.log( "Obesidad hipermórbida" );
     }
 }
-
