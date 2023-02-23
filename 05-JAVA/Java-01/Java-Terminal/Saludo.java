f949d4601af2d75407523bd6ec0449a6f50bfb2b
Ingrid Fonoy <ingridsfonoyd@gmail.com>
2023-02-23 17:38:56 -0600

    introToJAVA

---
+++ 05-JAVA/Java-01/Java-Terminal/Saludo.java
@@ -0,0 +1,9 @@
+/* esto es una clase comentario en block*/
+class Saludo
+{
+//esto es un metodo, comentario en linea
+	void saludar()
+	{
+		System.out.println("Hola de nuez desde java oop");
+	}
+}
