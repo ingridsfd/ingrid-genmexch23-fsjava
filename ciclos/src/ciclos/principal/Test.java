a57032fd98d0719176da6537370cf7ffdaca4085
Ingrid Fonoy <ingridsfonoyd@gmail.com>
2023-03-01 13:03:14 -0600

    ciclosJava

---
+++ ciclos/src/ciclos/principal/Test.java
@@ -0,0 +1,24 @@
+package ciclos.principal;
+
+import ciclos.forwhiledo.CicloDo;
+import ciclos.forwhiledo.UsoFor;
+//Recordemos importar
+import ciclos.forwhiledo.UsoWhile;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//UsoWhile objetoUW = new UsoWhile();
+		//UsoFor uf = new UsoFor();
+		CicloDo cd = new CicloDo();
+		//objetoUW.cicloW();
+		//objetoUW.centinelaW();
+		//objetoUW.banderaW();
+		//uf.cicloFor();
+		//uf.letrasfor();
+		cd.digitosDo();
+
+	}
+
+}
